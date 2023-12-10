8cd00c3dbb2eadc6f0dc6e9d1ad8c6a74f2ee66a
nebula <jlevidacombe@gmail.com>
2023-12-10 17:19:00 +1300

    initial upload

---
+++ src/RPTB/RPTB/Program.cs
@@ -0,0 +1,66 @@
+﻿using RPTB.Utilities;
+
+internal class Program
+{
+    private static void Main()
+    {
+        do
+        {
+            Console.Clear(); // Clear the console only once
+            DisplayAsciiArt("RPTB");
+
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Create Config");
+            Console.WriteLine("2. Add Reverse Proxy Entry");
+            Console.WriteLine("3. Start");
+            Console.WriteLine("4. Stop");
+            Console.WriteLine("5. Restart");
+            Console.WriteLine("6. Exit");
+
+            Console.Write("Enter Option: ");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.WriteLine("Creating Config");
+                    ConfigManager.CreateConfig();
+                    break;
+                case "2":
+                    Console.WriteLine("Adding Entry");
+                    ConfigManager.AddEntry();
+                    break;
+                case "3":
+                    Console.WriteLine("Starting...");
+                    break;
+                case "4":
+                    Console.WriteLine("Stopping...");
+                    break;
+                case "5":
+                    Console.WriteLine("Restarting...");
+                    break;
+                case "6":
+                    Console.WriteLine("Exiting...");
+                    Environment.Exit(0);
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+
+            Thread.Sleep(1000); // Optional delay
+        } while (true);
+    }
+
+    private static void DisplayAsciiArt(string text)
+    {
+        Console.WriteLine(@"
+_________________________________________ 
+\______   \______   \__    ___/\______   \
+ |       _/|     ___/ |    |    |    |  _/
+ |    |   \|    |     |    |    |    |   \
+ |____|_  /|____|     |____|    |______  /
+        \/                             \/ 
+");
+    }
+}
---
+++ src/RPTB/RPTB/Utilities/ConfigManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RPTB.Utilities
+{
+    public class ConfigManager
+    {
+        private const string FilePath = "./caddyfile";
+
+        public static void CreateConfig()
+        {
+            try
+            {
+                string caddyFileContent = GetCaddyFileContent();
+
+                // Write the content to the file
+                File.WriteAllText(FilePath, caddyFileContent);
+
+                // Format the entire Caddyfile
+                FormatCaddyfile();
+
+                Console.WriteLine("Caddyfile created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating Caddyfile: {ex.Message}");
+            }
+        }
+
+        public static void AddEntry()
+        {
+            try
+            {
+                string newEntry = GetNewEntry();
+
+                // Save the new entry to the file
+                File.AppendAllText(FilePath, newEntry);
+
+                // Format the entire Caddyfile
+                FormatCaddyfile();
+
+                Console.WriteLine("Custom text added as a new entry to Caddyfile successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating Caddyfile: {ex.Message}");
+            }
+        }
+
+        private static string GetCaddyFileContent()
+        {
+            StringBuilder caddyFileContent = new StringBuilder();
+
+            do
+            {
+                Console.Write("Enter subdomain (e.g., sub.domain.tld): ");
+                string subdomain = Console.ReadLine();
+
+                Console.Write("Enter IP address and port (e.g., 127.0.0.1:8095): ");
+                string ipAddressAndPort = Console.ReadLine();
+
+                caddyFileContent.AppendLine($"{subdomain} {{");
+                caddyFileContent.AppendLine($"    reverse_proxy {ipAddressAndPort}");
+                caddyFileContent.AppendLine("}");
+
+                Console.Write("Do you want to add another entry? (y/n): ");
+            } while (Console.ReadLine()?.ToLower() == "y");
+
+            return caddyFileContent.ToString();
+        }
+
+
+        private static string GetNewEntry()
+        {
+            Console.Write("Enter subdomain (e.g., sub.domain.tld): ");
+            string subdomain = Console.ReadLine();
+
+            Console.Write("Enter IP address and port (e.g., 127.0.0.1:8095): ");
+            string ipAddressAndPort = Console.ReadLine();
+
+            return $"{subdomain} {{\n    reverse_proxy {ipAddressAndPort}\n}}";
+        }
+
+        private static void FormatCaddyfile()
+        {
+            try
+            {
+                // Read the content of the file
+                string content = File.ReadAllText(FilePath);
+
+                // Use a regular expression to ensure only one newline between entries
+                string pattern = $@"{Regex.Escape(Environment.NewLine)}{{2,}}";
+                string replacement = Environment.NewLine + Environment.NewLine;
+                string formattedContent = Regex.Replace(content, pattern, replacement);
+
+                // Write the formatted content back to the file
+                File.WriteAllText(FilePath, formattedContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error formatting Caddyfile: {ex.Message}");
+            }
+        }
+    }
+}
